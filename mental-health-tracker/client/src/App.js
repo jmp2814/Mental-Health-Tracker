6e1a9a02c9b40f8ea0c754c6de4edd727ec5d21c
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-11-01 21:25:39 -0400

    added routes for login and signup

--- mental-health-tracker/client/src/App.js
+++ mental-health-tracker/client/src/App.js
@@ -8,6 +8,8 @@ import {
 
 import Home from "./Pages/Home/Home";
 import Help from "./pages/Help";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/SignUp/SignUp";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -21,6 +23,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/help" element={<Help />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </ApolloProvider>
