4b6eb2e4f9ab5048c60949dfdfe1e03cb11134dc
Jesse <ftsjess@gmail.com>
2022-11-03 16:30:20 -0400

    fixed routes and help page

---
+++ mental-health-tracker/client/src/pages/help/help.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Help() {
+    return (
+        <div>
+            <h1>Need Help </h1>
+
+            <div class="card">
+                <div class="card-body">
+                    The 988 Lifeline
+
+                    988 is now active across the United States.
+                    This new, shorter phone number will make it easier for people to remember and access mental health crisis services.
+                    (Please note, the previous 1-800-273-TALK (8255) number will continue to function indefinitely.)
+                    Click below to learn more about 988.
+                </div>
+            </div>
+
+        </div>
+    );
+
+}
+
