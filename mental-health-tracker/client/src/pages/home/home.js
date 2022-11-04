a8b08366795be22d8305050430c38a0bcfdec617
Jesse <ftsjess@gmail.com>
2022-11-03 20:21:05 -0400

    homepage layout

---
+++ mental-health-tracker/client/src/pages/home/home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function Help() {
+    return (
+        <div>
+            <h1>Mental Health Tracker</h1>
+            <div class="card">
+                <div class="card-body">
+                    app discription/ intro text
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    inspirational quote 1
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    inspirational quote 2
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    how it works
+                </div>
+            </div>
+        </div>
+    );
+
+}
