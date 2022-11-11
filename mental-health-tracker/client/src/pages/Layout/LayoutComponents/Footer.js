8547f1ad91c1a870f2b1fe1c6f00485d99a54179
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-11-10 19:02:46 -0500

    fixed merge conflict

--- mental-health-tracker/client/src/pages/Layout/LayoutComponents/Footer.js
+++ mental-health-tracker/client/src/pages/Layout/LayoutComponents/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import "./pages/styles.css"
 
 class Footer extends React.Component {
   render() {
--- mental-health-tracker/client/src/pages/Login/login.js
+++ mental-health-tracker/client/src/pages/Login/login.js
@@ -1,62 +1,61 @@
 import React, { FormEvent, useState } from "react";
-import "./pages/styles.css"
+import "./pages/styles.module.css";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleEmailChange = (e) => {
-        setEmail(e.currentTarget.value);
-      };
-    
-      const handlePasswordChange = (e) => {
-        setPassword(e.currentTarget.value);
-      };
-    
-      const handleFormSubmit = (e) => {
-        e.preventDefault();
-        setEmail("");
-        setPassword("");
-      };
+  const handleEmailChange = (e) => {
+    setEmail(e.currentTarget.value);
+  };
 
-      return (
-        <div className="d-flex align-items-center text-center">
-          <main className={`w-100 m-auto p-2`}>
-            <form onSubmit={handleFormSubmit}>
-              <img src="ttb-logos.jpeg" alt="Logo" className={``} />
-              <h1 className="h3 mb-3 fw-normal">Login Here:</h1>
-              <div className="form-floating mb-2">
-                <input
-                  type="email"
-                  value={email}
-                  name="email"
-                  id="email"
-                  placeholder="email@email.com"
-                  onChange={handleEmailChange}
-                  className="form-control"
-                />
-                <label htmlFor="email">Email address:</label>
-              </div>
-              <div className="form-floating mb-2">
-                <input
-                  type="password"
-                  value={password}
-                  name="password"
-                  id="password"
-                  placeholder="password"
-                  onChange={handlePasswordChange}
-                  className="form-control"
-                />
-                <label htmlFor="password">Password:</label>
-              </div>
-              <button type="submit" className="w-100 btn btn-lg btn-primary">
-                Login
-              </button>
-            </form>
-          </main>
-        </div>
-      );
-    
+  const handlePasswordChange = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="d-flex align-items-center text-center">
+      <main className={`w-100 m-auto p-2`}>
+        <form onSubmit={handleFormSubmit}>
+          <img src="ttb-logos.jpeg" alt="Logo" className={``} />
+          <h1 className="h3 mb-3 fw-normal">Login Here:</h1>
+          <div className="form-floating mb-2">
+            <input
+              type="email"
+              value={email}
+              name="email"
+              id="email"
+              placeholder="email@email.com"
+              onChange={handleEmailChange}
+              className="form-control"
+            />
+            <label htmlFor="email">Email address:</label>
+          </div>
+          <div className="form-floating mb-2">
+            <input
+              type="password"
+              value={password}
+              name="password"
+              id="password"
+              placeholder="password"
+              onChange={handlePasswordChange}
+              className="form-control"
+            />
+            <label htmlFor="password">Password:</label>
+          </div>
+          <button type="submit" className="w-100 btn btn-lg btn-primary">
+            Login
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default Login;
