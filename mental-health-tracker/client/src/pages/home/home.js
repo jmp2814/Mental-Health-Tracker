1634c73c65e1537d107933d5d6a870cdfe8b70cc
Jesse <ftsjess@gmail.com>
2022-11-11 20:27:17 -0500

    home fixed

--- mental-health-tracker/client/src/pages/home/home.js
+++ mental-health-tracker/client/src/pages/home/home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import QuoteContainer from "../Components/quote";
 
-export default function Home() {
+
+export default function home() {
   return (
     <div>
       <h1>Mental Health Tracker</h1>
       <div className="card">
         <div className="card-body">app discription/ intro text</div>
       </div>
-
       <div className="card">
         <div className="card-body">
           <QuoteContainer />
