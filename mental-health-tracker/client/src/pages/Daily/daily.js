26c3ccf3d4047eaba111385f1c723ac31704a681
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-11-11 21:49:40 -0500

    moved fonts folder and got imports to work

--- mental-health-tracker/client/src/pages/Daily/daily.js
+++ mental-health-tracker/client/src/pages/Daily/daily.js
@@ -1,7 +1,7 @@
 import { useMutation } from "@apollo/client";
 import React, { FormEvent, useState } from "react";
 import { ADD_RESPONSE } from "../../utils/mutations";
-import "././public/fonts/Nunito_Sans/NunitoSans-Regular.ttf";
+import "../fonts/Nunito_Sans/NunitoSans-Regular.ttf";
 
 const EntryForm = () => {
   const [depression, setDepression] = useState(3);
